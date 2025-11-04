298c08863d15b7cc33e5d7e3acb9ebab4af8e728
ximlor <ximlor@ximlordeMacBook-Pro.local>
2025-11-04 22:51:34 +0300

    fix hint

--- .hammerspoon/modules/hint.lua
+++ .hammerspoon/modules/hint.lua
@@ -2,17 +2,13 @@ local hotkey = require 'hs.hotkey'
 local hints = require 'hs.hints'
 local window = require 'hs.window'
 
--- hints --------------------------------------------------
-
--- TODO:
--- 使用程序名称首字母作为快捷键存在系统应用无字母问题
---hints.style = 'vimperator'
-
-hotkey.bind('Ctrl', 'up', function()
+-- 窗口提示
+hotkey.bind('Ctrl', '[', function()
     hints.windowHints()
 end)
 
-hotkey.bind('Ctrl', 'down', function()
+-- 应用内窗口提示
+hotkey.bind('Ctrl', ']', function()
     hints.windowHints(window.focusedWindow():application():allWindows())
 end)
 
