c4d3d1c3719e0f8b18faca77e738e652e9480f2c
ximlor <ximlor@ximlordeMacBook-Pro.local>
2025-11-23 20:18:08 +0300

    fix

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,13 +7,12 @@ hyperShift = { 'ctrl', 'alt', 'cmd', 'shift' }
 
 key2App = {
     b = 'Google Chrome',
-    d = 'Visual Studio Code',
+    k = 'Visual Studio Code',
     i = 'iTerm',
     e = '印象笔记',
     f = 'Finder',
     o = 'Obsidian',
     w = 'wpsoffice',
-    p = 'TencentDocs',
 }
 -- 上面的应用大部分都可以使用应用名称，但个别应用（如 wps）需要与 ~/applications 中的应用名称一致
 
